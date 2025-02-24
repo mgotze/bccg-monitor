2071fd4bf00ce51e020d277cf78b90c33bafcf24
Marcos Gotze <mgotze@gmail.com>
2025-02-24 03:22:16 -0300

    Update resultModel.js

--- src/models/resultModel.js
+++ src/models/resultModel.js
@@ -1,1 +1,20 @@
+const mongoose = require('mongoose');
+
+const ResultSchema = new mongoose.Schema(
+  {
+    crashPoint: {
+      type: Number,
+      required: true
+    },
+    timestamp: {
+      type: Date,
+      default: Date.now
+    }
+  },
+  {
+    versionKey: false
+  }
+);
+
+module.exports = mongoose.model('Result', ResultSchema);
 
